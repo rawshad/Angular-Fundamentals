57a5823a43918cc63595612fe1e1b78d7bf5edf6
rawshad <rawshad@outlook.com>
2023-01-14 21:48:04 +0600

    ngIf

---
+++ rendering-flows/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `<div class="">
+    <input type="text" 
+    [value]="name" (input)= "handleInput($event)">
+    <!-- <template [ngIf]="name.length > 2"]>
+    <div>Searching for ... {{name}}</div>
+    </template> -->
+    <div *ngIf="name.length > 2">Searching for ... {{name}}</div>
+  </div>`
+})
+export class AppComponent {
+  name: string = "";
+  handleInput(event: any) {
+    this.name = event.target.value;
+  }
+}
